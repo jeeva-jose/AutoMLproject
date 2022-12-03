7a15fe79acf29b70fea2d02830e7ae2aafc7785e
jeeva-jose <118467978+jeeva-jose@users.noreply.github.com>
2022-12-03 16:38:22 +0530

    Add files via upload

---
+++ endpoint.py
@@ -0,0 +1,82 @@
+import requests
+import json
+
+# URL for the web service, should be similar to:
+# 'http://8530a665-66f3-49c8-a953-b82a2d312917.eastus.azurecontainer.io/score'
+scoring_uri = 'http://b8628bf7-17ca-4566-8168-4b8dfbdd7191.southcentralus.azurecontainer.io/score'
+# If the service is authenticated, set the key or token
+key = 'bAs7LsKtTEsdFojw2hQKh6kDonAESDfc'
+
+# Two sets of data to score, so we get two results back
+
+data =  {
+  "Inputs": {
+    "data": [
+      {
+            "age": 17,
+            "job": "blue-collar",
+            "marital": "married",
+            "education": "university.degree",
+            "default": "no",
+            "housing": "yes",
+            "loan": "yes",
+            "contact": "cellular",
+            "month": "may",
+            "day_of_week": "mon",
+            "duration": 971,
+            "campaign": 1,
+            "pdays": 999,
+            "previous": 1,
+            "poutcome": "failure",
+            "emp.var.rate":-1.8,
+            "cons.price.idx": 92.893,
+            "cons.conf.idx": -46.2,
+            "euribor3m": 1.299,
+            "nr.employed": 5099.1
+          },
+           {
+            "age": 87,
+            "campaign": 1,
+            "cons.conf.idx": -46.2,
+            "cons.price.idx": 92.893,
+            "contact": "cellular",
+            "day_of_week": "mon",
+            "default": "no",
+            "duration": 471,
+            "education": "university.degree",
+            "emp.var.rate": -1.8,
+            "euribor3m": 1.299,
+            "housing": "yes",
+            "job": "blue-collar",
+            "loan": "yes",
+            "marital": "married",
+            "month": "may",
+            "nr.employed": 5099.1,
+            "pdays": 999,
+            "poutcome": "failure",
+            "previous": 1
+      }
+    ]
+  },
+  "GlobalParameters": {
+    "method": "predict"
+  }
+}
+
+
+
+# Convert to JSON string
+input_data = json.dumps(data)
+with open("data.json", "w") as _f:
+    _f.write(input_data)
+
+# Set the content type
+headers = {'Content-Type': 'application/json'}
+# If authentication is enabled, set the authorization header
+headers['Authorization'] = f'Bearer {key}'
+
+# Make the request and display the response
+resp = requests.post(scoring_uri, input_data, headers=headers)
+print(resp.json())
+
+
